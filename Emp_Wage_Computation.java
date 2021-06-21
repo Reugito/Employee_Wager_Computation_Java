a537b1f194910bd3f8aae402ecc920152c2f0782
Reugito <65726495+Reugito@users.noreply.github.com>
2021-06-21 14:00:35 +0530

    Employee monthly salary

---
+++ Emp_Wage_Computation.java
@@ -0,0 +1,35 @@
+package uc5_Calculate_Emp_Wage_For_Month;
+
+import uc4_Wager_Using_Switch_Case.SwitchCase;
+
+public class Monthly_Wages extends SwitchCase {
+
+	public static void main(String[] args) {
+		
+		int TotalWage = 0;
+		
+		for(int Day = 1; Day <= 30; Day++) {
+		
+			int check = Switch();
+			
+			switch (check) {
+			
+			case 1:                                                        // full time wage
+				TotalWage += Wager();     // Daily wager
+				break;
+				
+			case 2:                                                        // part time wage
+			
+				TotalWage += PartTimeWage() + Wager();
+				break;
+				
+			default:                                                    //Absent
+
+               TotalWage += 0;
+			}
+		}
+
+		System.out.println(" Employee Monthly wager = "+TotalWage);
+	}
+
+}
