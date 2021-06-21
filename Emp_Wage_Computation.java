b2d065b4587f83aecf05e71515da7375bf19a95c
Reugito <65726495+Reugito@users.noreply.github.com>
2021-06-21 13:31:49 +0530

    Employee part time wager

--- Emp_Wage_Computation.java
+++ Emp_Wage_Computation.java
@@ -1,41 +1,45 @@
-
-package uc3.PartTime.Emp.Wage;
+package uc3_PartTime_Emp_Wage;
 
 import java.util.Random;
 
-import uc2.Calculate.Daily.Employee.Wage.Daily_Wage;
+import uc2_Calculate_Daily_Employee_Wage.Daily_Wage;
 
-public class PartTime_Wage extends Daily_Wage {
+public class PartTime_Wage extends Daily_Wage {   // Adding partime employee
 	
-	public static int Attendance() {                 // adding part time employee
+	public static int Attendance() {
 		
-		int isPresent = 1, isParttime = 2, isAbsent=0;
-		Random random = new Random();                  
+		Random random = new Random();
 		int check = random.nextInt(3);
-		
 		return check;
 	}
 	
-    public static int PartTimeWage() {                 // adding part time wager
+    public static int PartTimeWage() {             // part time wager
     	
-    	int isParttime = 2, WagePerHr = 20, PartThimeHr = 8;
-    	int attendance = Attendance();             // called static method of class without creating object
-    	
-    	if (attendance == isParttime) {
-			int PartTimeWage = WagePerHr * PartThimeHr;
-			return PartTimeWage;
-		}
-    	return 0;
-    	
+    	int WagePerHr = 20, PartThimeHr = 4;
+    	int PartTimeWage = WagePerHr * PartThimeHr;
+        return PartTimeWage;
     }
-	public static void main(String[] args) {                  // calculating Total wager
-		
-		int parttimeWage = PartTimeWage();
-		int dailyWage = Wager();                    // Daily wager of Emp from Daily_wage class
-		int TotalWage = parttimeWage + dailyWage;   // Adding part time wage to daily wage
-		System.out.println("Employee daily Total Daily wage = "+TotalWage);
-		
     
-	}
+	public static void main(String[] args) {
+		
+		int isPresent = 1, isParttime = 2, isAbsent=0;
+		
+		int attendance = Attendance();  
+		int dailyWage = Wager();                    
+		
+		if (attendance == isPresent) {
+			
+			System.out.print("Employee Full day wage = "+dailyWage);
+			}
+			
+		else if(attendance == isParttime) {
 
+			System.out.println("Employee Part time wage = "+PartTimeWage());
+			}
+		
+		else {
+			
+			System.out.println("Employee Daily Wager = "+isAbsent);
+		}
+	}
 }
