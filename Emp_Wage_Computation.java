81b37d5501a519ef2ec333bf8ad711598b2dad90
Raosaheb Dhotre <65726495+Reugito@users.noreply.github.com>
2021-06-22 08:41:38 +0530

    Using switch case

--- Emp_Wage_Computation.java
+++ Emp_Wage_Computation.java
@@ -3,7 +3,10 @@ package uc4_Wager_Using_Switch_Case;
 import java.util.*;
 
 import uc3_PartTime_Emp_Wage.PartTime_Wage;
+
 public class SwitchCase extends PartTime_Wage {
+	
+	public static final int isFulltime = 1, isParttime = 2;
 	
 	public static int Switch() {
 		
@@ -11,18 +14,18 @@ public class SwitchCase extends PartTime_Wage {
 		int check = randome.nextInt(3);
 		return check;
 	}
-
 	public static void main(String[] args) {
 		
 		int check = Switch();
 		
 		switch (check) {
 		
-		case 1:                                                        // full time wage
+		case isFulltime: 					// full time wage
+			
 			System.out.println("Employee Full day Wager = "+Wager());
 			break;
 			
-		case 2:                                                        // part time wage
+		case isParttime:                                                        // part time wage
 		
 			System.out.println("Employee Part time Wager = "+PartTimeWage());
 			break;
@@ -32,3 +35,4 @@ public class SwitchCase extends PartTime_Wage {
 		
 	}
 
+}
